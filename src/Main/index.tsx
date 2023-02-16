4551a223097a031f36bab65419dfa7cf7fdee37f
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-02-16 18:34:33 -0300

    chore: create base to home screen

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,11 +1,31 @@
+import { Categories } from "../components/Categories";
 import { Header } from "../components/Header";
+import { Menu } from "../components/Menu";
 
-import { Container } from "./styles";
+import {
+  Container,
+  CategoriesContainer,
+  MenuContainer,
+  Footer,
+  FooterContent,
+} from "./styles";
 
 export function Main() {
   return (
-    <Container>
-      <Header />
-    </Container>
+    <>
+      <Container>
+        <Header />
+        <CategoriesContainer>
+          <Categories />
+        </CategoriesContainer>
+
+        <MenuContainer>
+          <Menu />
+        </MenuContainer>
+      </Container>
+      <Footer>
+        <FooterContent></FooterContent>
+      </Footer>
+    </>
   );
 }
